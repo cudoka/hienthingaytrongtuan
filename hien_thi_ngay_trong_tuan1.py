e5b68182e1252bcb380c944a263663fda5d7334b
cudoka <damthang7594@gmail.com>
2022-10-31 17:48:33 +0700

    khoi tao hien thi ngay trong tuan

---
+++ hien_thi_ngay_trong_tuan1.py
@@ -0,0 +1,17 @@
+thu = int(input("Nhập vào 1 số (từ 1 đến 7): "))
+if(thu == 1):
+    print("Thứ 2")
+elif(thu == 2):
+    print("Thứ 3")
+elif(thu == 3):
+    print("Thứ 4")
+elif(thu == 4):
+    print("Thứ 5")
+elif(thu == 5):
+    print("Thứ 6")
+elif(thu == 6):
+    print("Thứ 7")
+elif(thu == 7):
+    print("Chủ nhật")
+else:
+    print("Lỗi, không phải thứ trong tuần")
